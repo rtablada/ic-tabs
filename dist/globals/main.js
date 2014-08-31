f96ef058f02add48efcf819ec9d9152de4568ed1
Ryan Tablada <ryan.tablada@gmail.com>
2014-08-31 18:07:48 -0400

    Allow for text indexes... This is not final

--- dist/globals/main.js
+++ dist/globals/main.js
@@ -465,8 +465,8 @@ exports["default"] = Component.extend({
   selectFromTabsSelectedIndex: function() {
     var activeTab = this.get('tabs.activeTab');
     if (activeTab === this) return; // this was just selected
-    var index = parseInt(this.get('tabs.selected-index'), 10);
-    var myIndex = this.get('index');
+    var index = this.get('tabs.selected-index').toString();
+    var myIndex = this.get('index').toString();
     if (index === myIndex) {
       this.select();
     }
@@ -495,11 +495,11 @@ var Ember = window.Ember["default"] || window.Ember;
 exports["default"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  
+
 
 
   data.buffer.push("ic-tabs,\nic-tab-list,\nic-tab-panel {\n  display: block\n}\n\nic-tab-list {\n  border-bottom: 1px solid #aaa;\n}\n\nic-tab {\n  display: inline-block;\n  padding: 6px 12px;\n  border: 1px solid transparent;\n  border-top-left-radius: 3px;\n  border-top-right-radius: 3px;\n  cursor: pointer;\n  margin-bottom: -1px;\n  position: relative;\n}\n\nic-tab[selected] {\n  border-color: #aaa;\n  border-bottom-color: #fff;\n}\n\nic-tab:focus {\n  box-shadow: 0 10px 0 0 #fff,\n              0 0 5px hsl(208, 99%, 50%);\n  border-color: hsl(208, 99%, 50%);\n  border-bottom-color: #fff;\n  outline: none;\n}\n\nic-tab:focus:before,\nic-tab:focus:after {\n  content: '';\n  position: absolute;\n  bottom: -6px;\n  width: 5px;\n  height: 5px;\n  background: #fff;\n}\n\nic-tab:focus:before {\n  left: -4px;\n}\n\nic-tab:focus:after {\n  right: -4px;\n}\n\n");
-  
+
 });
 },{}],6:[function(_dereq_,module,exports){
 "use strict";
